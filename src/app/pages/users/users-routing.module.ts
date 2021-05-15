d8a0849cd9d37e90742ea25429f30eef16ae061a
Lucas Tiago de Moraes <lucastiagodemoraes@gmail.com>
2021-05-15 11:33:04 -0300

    Added component user-list

--- src/app/pages/users/users-routing.module.ts
+++ src/app/pages/users/users-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// components
+import { UserListComponent } from './user-list/user-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UserListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 
+// modules
+import { SharedModule } from '../../shared/shared.module';
+
+// components
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserListComponent
+  ],
   imports: [
     CommonModule,
+    SharedModule,
     UsersRoutingModule
   ]
 })
