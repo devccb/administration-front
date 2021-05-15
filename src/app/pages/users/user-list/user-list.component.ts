af8f2fca4a375a5e6bd3dab274e3b780b8de517d
Lucas Tiago de Moraes <lucastiagodemoraes@gmail.com>
2021-05-15 13:15:43 -0300

    Users (#6)
    * Added component user-list
    
    * Added user model and no more tables
    
    * Added user-form and added router

--- src/app/pages/users/user-list/user-list.component.ts
+++ src/app/pages/users/user-list/user-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-user-list',
+  selector: 'users-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
