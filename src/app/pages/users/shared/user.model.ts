da8e3a44217523bc619f0060129eef27d0a1a559
Lucas Tiago de Moraes <lucastiagodemoraes@gmail.com>
2021-05-15 12:59:08 -0300

    Added user model and no more tables

---
+++ src/app/pages/users/shared/user.model.ts
@@ -0,0 +1,13 @@
+import { BaseModel } from "../../../shared/models/base.model";
+
+export class User extends BaseModel{
+  constructor(
+    public name?: string,
+    public email?: string,
+    public login?: string,
+    public password?: string,
+    public status?: number,
+  ){
+    super();
+  }
+}
